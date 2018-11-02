2913fb6ab985c6dc86ffdec5fca4a0375c0eca39
Alberto Santos Arenillas <alberto.santos@sstrategy.io>
2018-11-02 11:33:45 +0100

    Ejemplo de publicación de mensaje en una cola funcionando.

---
+++ src/main/java/com/sstrategy/demo/Email.java
@@ -0,0 +1,47 @@
+package com.sstrategy.demo;
+
+import java.io.Serializable;
+
+/**
+ * Un POJO para ser publicado en forma de mensaje JMS.
+ */
+public class Email implements Serializable {
+
+	/**
+	 * Serial version UID autogenerado, para poder convertir este objeto en un mensaje JMS.
+	 */
+	private static final long serialVersionUID = -3579805425262712807L;
+	
+	private String to;
+	private String body;
+
+	public Email() {
+	}
+
+	public Email(String to, String body) {
+		this.to = to;
+		this.body = body;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Email{to=%s, body=%s}", getTo(), getBody());
+	}
+
+}
--- src/main/java/com/sstrategy/demo/controlador/DemoController.java
+++ src/main/java/com/sstrategy/demo/controlador/DemoController.java
@@ -5,20 +5,29 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.NoSuchMessageException;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sstrategy.demo.Email;
 
 @RestController
 @RequestMapping(value = "/demo")
 public class DemoController {
 
+	@Autowired
+	JmsTemplate jmsTemplate;
+	
 	@RequestMapping(value = "", method = RequestMethod.GET, produces = "text/plain; charset=UTF-8")
 	public String estableceCorreoPorDefecto(
 			HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
 			throws NoSuchMessageException, IOException {
 		
+		jmsTemplate.convertAndSend("mailbox", new Email("info@example.com", "Hello"));
+	
 		return "Hello world!";
 	}
 }
